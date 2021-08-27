1d3a4aec3fc3602a9c25d6120b595de3159ca2a1
Ray-Shakex <bshakex@gmail.com>
2021-08-27 11:51:43 +0200

    app.js

--- app.js
+++ app.js
@@ -5,3 +5,16 @@
 //   Pocket borealis
 //   The curiosity
 //   Make it fisheye
+
+
+
+// hamburger menu toggle
+// select hamburger icon and navlist to be displayed
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('#header .navbar .mobile-list');
+console.log(navList);
+// add event lister to toggle active class on hamburger on click
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle('active');
+    navList.classList.toggle('.active');
+});
