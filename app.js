8941ab8cb2332f84f765aaf99279a32e52ddf8d1
Ray-Shakex <bshakex@gmail.com>
2021-08-27 13:53:30 +0200

    hamburger toggle

--- app.js
+++ app.js
@@ -12,9 +12,18 @@
 // select hamburger icon and navlist to be displayed
 const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('#header .navbar .mobile-list');
-console.log(navList);
+const navItem = document.querySelectorAll('#header .navbar .mobile-list li a');
+console.log(navItem);
 // add event lister to toggle active class on hamburger on click
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle('active');
-    navList.classList.toggle('.active');
+    navList.classList.toggle('active');
 });
+
+// remove nav-list when item selected
+navItem.forEach(item => {
+    item.addEventListener("click" , () => {
+        navList.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    })
+})
