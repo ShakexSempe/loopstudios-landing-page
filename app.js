df55038909b3e8c6c97762b089db84d3a71e668f
Ray-Shakex <bshakex@gmail.com>
2021-08-27 14:36:23 +0200

    app.js work array and siplay galleries

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// Night arcade
-//   Soccer team VR
-//   The grid
-//   From up above VR
-//   Pocket borealis
-//   The curiosity
-//   Make it fisheye
-
 
 
 // hamburger menu toggle
@@ -25,5 +17,99 @@ navItem.forEach(item => {
     item.addEventListener("click" , () => {
         navList.classList.toggle('active');
         hamburger.classList.toggle('active');
-    })
-})
+    });
+});
+
+
+//   
+//   
+
+
+// gallery
+const desktop = document.querySelector('.desktop-gallery');
+const mobile = document.querySelector('.mobile-gallery');
+
+const work = [
+    {
+        id: 1,
+        title: "deep earth",
+        desktop: "images/desktop/image-deep-earth.jpg",
+        mobile: "images/mobile/image-deep-earth.jpg",
+    },
+    {
+        id: 2,
+        title: "Night arcade",
+        desktop: "images/desktop/image-night-arcade.jpg",
+        mobile: "images/mobile/image-night-arcade.jpg",
+    },
+    {
+        id: 3,
+        title: "Soccer team VR",
+        desktop: "images/desktop/image-soccer-team.jpg",
+        mobile: "images/mobile/image-soccer-team.jpg",
+    },
+    {
+        id: 4,
+        title: "The grid",
+        desktop: "images/desktop/image-grid.jpg",
+        mobile: "images/mobile/image-grid.jpg",
+    },
+    {
+        id: 5,
+        title: "From up above VR",
+        desktop: "images/desktop/image-from-above.jpg",
+        mobile: "images/mobile/image-from-above.jpg",
+    },
+    {
+        id: 6,
+        title: "Pocket borealis",
+        desktop: "images/desktop/image-pocket-borealis.jpg",
+        mobile: "images/mobile/image-pocket-borealis.jpg",
+    },
+    {
+        id: 7,
+        title: "The curiosity",
+        desktop: "images/desktop/image-curiosity.jpg",
+        mobile: "images/mobile/image-curiosity.jpg",
+    },
+    {
+        id: 8,
+        title: "Make it fisheye",
+        desktop: "images/desktop/image-fisheye.jpg",
+        mobile: "images/mobile/image-fisheye.jpg",
+    },
+    
+];
+
+// window event lister : display desktop gallery
+window.addEventListener('DOMContentLoaded' , () => {
+    desktopGallery(work);
+    mobileGallery(work);
+});
+
+// desktopGallery 
+desktopGallery = (workItems) => {
+    let displayItem = workItems.map(item => {
+        return `<figure>
+            <img src=${item.desktop} alt="img">
+            <figcaption>${item.title}</figcaption>
+        </figure>`
+    });
+    displayItem = displayItem.join("");
+    desktop.innerHTML = displayItem;
+
+}
+
+// mobileGallery
+mobileGallery = (workItems) => {
+    let displayItem = workItems.map(item => {
+        return `<figure>
+            <img src=${item.mobile} alt="img">
+            <figcaption>${item.title}</figcaption>
+        </figure>`
+    });
+    displayItem = displayItem.join("");
+    mobile.innerHTML = displayItem;
+}
+
+
